e939275220173f7bd390183888ced819220484ae
ihza6661 <ihzahmahendra6661@gmail.com>
2025-12-14 00:59:26 +0700

    Fix: Add Cloudinary to Next.js image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
       {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
